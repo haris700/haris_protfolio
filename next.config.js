510a6be2af2bfcabd7e7769bed013fc889421f2a
haris700 <Harisvkvnr@gmail.com>
2026-01-15 12:53:09 +0530

    Built with precision.

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "haris_portfolio",
+  assetPrefix: "/haris_portfolio/",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
